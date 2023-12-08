14e5255ba4e4194e3b7a6ef154939d6a3cf3d36d
game-cookie <a48639315@gmail.com>
2023-12-08 16:05:00 +0800

    https secue2

--- controller/Users.js
+++ controller/Users.js
@@ -50,7 +50,7 @@ export const login=async(req,res)=>{
             }
         })
         res.cookie('refreshToken',refreshToken,{
-            httpOnly:true,
+            // httpOnly:true,
             maxAge:24*60*60*1000,
             secure:true
         })
