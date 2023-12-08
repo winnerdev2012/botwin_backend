8c83db25f0923c80ca7fbf012da4371d0546f8f0
game-cookie <a48639315@gmail.com>
2023-12-08 15:03:35 +0800

    cors change

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ try {
     console.error('Connection Error:',error)
 }
 
-app.use(cors({credentials:true,origin:'http://localhost:3000'}))
+app.use(cors({credentials:true,origin:'https://botwin-frontend.vercel.app'}))
 app.use(cookieParser())
 app.use(express.json())
 app.use('/',authRoutes)
