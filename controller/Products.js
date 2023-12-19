e31d88caeea041c18b7696cb0f0b2562fa7b5e86
game-cookie <a48639315@gmail.com>
2023-12-19 22:10:24 +0800

    backend updated

--- controller/Products.js
+++ controller/Products.js
@@ -17,8 +17,23 @@ const getActionListByChainId = async (req, res) => {
 const getChainList = async (req, res) => {
     try {
         const result = await db.query(`
-            SELECT chain_id, chain_name, chain_active
+            SELECT chain_id, chain_name, chain_active, max_action_count
             FROM chain_list;
+        `, { type: Sequelize.QueryTypes.SELECT });
+        res.json(result);
+    } catch (error) {
+        res.json({ message: error.message });
+    }
+}
+
+const getChainList2 = async (req, res) => {
+    try {
+        const data = req.params.id.split(',')
+        const ids = data.map(Number)
+        const result = await db.query(`
+            SELECT chain_id, chain_name
+            FROM chain_list
+            WHERE chain_id IN (${ids});
         `, { type: Sequelize.QueryTypes.SELECT });
         res.json(result);
     } catch (error) {
@@ -29,7 +44,7 @@ const getChainList = async (req, res) => {
 const getBridgeList = async (req, res) => {
     try {
         const result = await db.query(`
-            SELECT bridge_id, bridge_name, bridge_url
+            SELECT bridge_id, bridge_name, bridge_url, bridge_weight
             FROM bridge_list;
         `, { type: Sequelize.QueryTypes.SELECT });
         res.json(result);
@@ -37,6 +52,7 @@ const getBridgeList = async (req, res) => {
         res.json({ message: error.message });
     }
 }
+
 
 const getScriptList = async (req, res) => {
     try {
@@ -62,11 +78,55 @@ const getActionById = async (req, res) => {
         res.json({ message: error.message });
     }
 }
+
+const getAvailableBridge = async (req, res) => {
+    try {
+        const result = await db.query(`
+            SELECT bridge_list.bridge_id, bridge_list.bridge_name, bridge_list.bridge_url, bridge_join.previous_chain_id
+            FROM bridge_join
+            JOIN bridge_list ON bridge_join.able_bridge_id = bridge_list.bridge_id
+            WHERE bridge_join.current_chain_id = ${req.params.id};
+        `, { type: Sequelize.QueryTypes.SELECT });
+        res.json(result);
+    } catch (error) {
+        res.json({ message: error.message });
+    }
+}
+
+const getChainListByBridgeList = async (req, res) => {
+    try {
+        const result = await db.query(`
+            SELECT DISTINCT cl.chain_id, cl.chain_name
+            FROM chain_list cl
+            WHERE cl.chain_id IN (
+                SELECT DISTINCT bj.able_chain_id AS chain_id
+                FROM bridge_join bj
+                WHERE bj.able_bridge_id = ${req.params.id}
+            )
+        `, { type: Sequelize.QueryTypes.SELECT });
+        res.json(result);
+    } catch (error) {
+        res.json({ message: error.message });
+    }
+}
+
+
 const addActionItem = async (req, res) => {
     try {
         const result = await db.query(`
-            INSERT INTO action_list (action_name, action_url, action_type, chain_id, action_weight)
-            VALUES ('${req.body.action_name}', '${req.body.action_url}', ${req.body.action_type}, ${req.body.chain_id}, ${req.body.action_weight});
+            INSERT INTO action_list (action_name, action_url, action_type, chain_id, action_weight, active)
+            VALUES ('${req.body.action_name}', '${req.body.action_url}', ${req.body.action_type}, ${req.body.chain_id}, ${req.body.action_weight}, "1 1 1 1");
+        `, { type: Sequelize.QueryTypes.SELECT });
+        res.json(result);
+    } catch (error) {
+        res.json({ message: "Create success" });
+    }
+}
+const addBridgeItem = async (req, res) => {
+    try {
+        const result = await db.query(`
+            INSERT INTO bridge_list (bridge_name, bridge_url, bridge_weight)
+            VALUES ('${req.body.bridge_name}', '${req.body.bridge_url}', ${req.body.bridge_weight});
         `, { type: Sequelize.QueryTypes.SELECT });
         res.json(result);
     } catch (error) {
@@ -84,6 +144,19 @@ const addChainItem = async (req, res) => {
         res.json({ message: "Create success" });
     }
 }
+
+const addBridgeJoin = async (req, res) => {
+    try {
+        const result = await db.query(`
+            INSERT INTO bridge_join ( able_chain_id, able_bridge_id )
+            VALUES (${req.body.chain_id}, ${req.body.bridge_id});
+        `, { type: Sequelize.QueryTypes.SELECT });
+        res.json(result);
+    } catch (error) {
+        res.json({ message: "Create success" });
+    }
+}
+
 const updateActionItem = async (req, res) => {
     try {
         const result = await db.query(`
@@ -96,11 +169,11 @@ const updateActionItem = async (req, res) => {
         res.json({ message: "Update success" });
     }
 }
-const setActiveAction = async (req, res) => {
+const updateActiveAction = async (req, res) => {
     try {
         const result = await db.query(`
         UPDATE action_list
-        SET active = ${req.body.isActive}
+        SET active = '${req.body.new_values}'
         WHERE action_id = ${req.body.action_id};
         `, { type: Sequelize.QueryTypes.SELECT });
         res.json(result);
@@ -108,11 +181,25 @@ const setActiveAction = async (req, res) => {
         res.json({ message: "Update success" });
     }
 }
-const setActiveChain = async (req, res) => {
+
+const updateBridge = async (req, res) => {
+    try {
+        const result = await db.query(`
+        UPDATE bridge_list
+        SET bridge_name = '${req.body.bridge_name}', bridge_url = '${req.body.bridge_url}', bridge_weight = '${req.body.bridge_weight}'
+        WHERE bridge_id = ${req.body.bridge_id};
+        `, { type: Sequelize.QueryTypes.SELECT });
+        res.json(result);
+    } catch (error) {
+        res.json({ message: "Update success" });
+    }
+}
+
+const updateActiveChain = async (req, res) => {
     try {
         const result = await db.query(`
         UPDATE chain_list
-        SET chain_active = ${req.body.isActive}
+        SET chain_active = '${req.body.new_values}'
         WHERE chain_id = ${req.body.chain_id};
         `, { type: Sequelize.QueryTypes.SELECT });
         res.json(result);
@@ -120,6 +207,20 @@ const setActiveChain = async (req, res) => {
         res.json({ message: "Update success" });
     }
 }
+
+const updateMaxActionCount = async (req, res) => {
+    try {
+        const result = await db.query(`
+        UPDATE chain_list
+        SET max_action_count = '${req.body.max_action_count}'
+        WHERE chain_id = ${req.body.chain_id};
+        `, { type: Sequelize.QueryTypes.SELECT });
+        res.json(result);
+    } catch (error) {
+        res.json({ message: "Update success" });
+    }
+}
+
 const deleteSelectedItem = async (req, res) => {
     try {
         const data = req.params.id.split(',')
@@ -144,4 +245,54 @@ const deleteChain = async (req, res) => {
         res.json({ message: "Delete success" })
     }
 }
-export { addActionItem, addChainItem, getActionById, getBridgeList, updateActionItem, setActiveAction, setActiveChain, deleteSelectedItem, deleteChain, getActionListByChainId, getChainList, getScriptList }
+
+const deleteBridge = async (req, res) => {
+    try {
+        const result = await db.query(`
+            DELETE FROM bridge_list
+            WHERE bridge_id IN (${req.params.id});
+        `, { type: Sequelize.QueryTypes.SELECT });
+        res.json(result);
+    } catch (error) {
+        res.json({ message: "Delete success" })
+    }
+}
+
+const deleteChainFromBridge = async (req, res) => {
+    try {
+        const result = await db.query(`
+            DELETE FROM bridge_join
+            WHERE able_chain_id = ${req.body.chain_id} and able_bridge_id = ${req.body.bridge_id}
+        `, { type: Sequelize.QueryTypes.SELECT });
+        res.json(result);
+    } catch (error) {
+        res.json({ message: "Delete success" })
+    }
+}
+
+export {
+    addActionItem,
+    addChainItem,
+    addBridgeItem,
+    addBridgeJoin,
+
+    getActionById,
+    getBridgeList,
+    getChainList2,
+    getAvailableBridge,
+    getActionListByChainId,
+    getChainListByBridgeList,
+    getChainList,
+    getScriptList,
+
+    updateActionItem,
+    updateMaxActionCount,
+    updateActiveAction,
+    updateActiveChain,
+    updateBridge,
+
+    deleteSelectedItem,
+    deleteChain,
+    deleteBridge,
+    deleteChainFromBridge
+}
--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,56 @@
 import express from 'express'
-import { addActionItem, addChainItem, getActionById, setActiveAction, setActiveChain, getScriptList, getBridgeList, updateActionItem, deleteSelectedItem, deleteChain, getActionListByChainId, getChainList } from '../controller/Products.js'
+import {
+    addActionItem,
+    addChainItem,
+    addBridgeItem,
+    addBridgeJoin,
+
+    updateActionItem,
+    updateActiveAction,
+    updateActiveChain,
+    updateMaxActionCount,
+    updateBridge,
+    
+    getActionById,
+    getScriptList,
+    getAvailableBridge,
+    getChainListByBridgeList,
+    getBridgeList,
+    getChainList2,
+    getActionListByChainId,
+    getChainList,
+
+    deleteSelectedItem,
+    deleteChain,
+    deleteBridge,
+    deleteChainFromBridge
+} from '../controller/Products.js'
 
 const router = express.Router()
 
 router.get('/get_action_list/:id', getActionListByChainId)
 router.get('/get_chain_list', getChainList)
+router.get('/get_chain_list2/:id', getChainList2)
 router.get('/get_script_list', getScriptList)
 router.get('/get_bridge_list', getBridgeList)
 router.get('/get_action_item/:id', getActionById)
+router.get('/get_available_bridge/:id', getAvailableBridge)
+router.get('/get_chain_list_by_bridge/:id', getChainListByBridgeList)
+
 router.post('/add_action_item', addActionItem)
 router.post('/add_chain_item', addChainItem)
+router.post('/add_bridge_item', addBridgeItem)
+router.post('/add_bridge_join', addBridgeJoin)
+
 router.patch('/update_action_item/:id', updateActionItem)
-router.patch('/set_avtive_action', setActiveAction)
-router.patch('/set_avtive_chain', setActiveChain)
+router.patch('/update_Bridge', updateBridge)
+router.patch('/set_avtive_action', updateActiveAction)
+router.patch('/set_avtive_chain', updateActiveChain)
+router.patch('/set_max_action_count', updateMaxActionCount)
+
 router.delete('/delete_selected_item/:id', deleteSelectedItem)
 router.delete('/delete_chain/:id', deleteChain)
+router.delete('/delete_bridge/:id', deleteBridge)
+router.delete('/delete_chain_from_bridge', deleteChainFromBridge)
 
 export default router
